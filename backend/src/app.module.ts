94970b5721aba6bbbc3d01ba76c0a4d0ccf5bc9f
Kwon Yong Hyun <takeallusername@gmail.com>
2026-02-24 16:42:49 +0900

    feat: Request ID 미들웨어 구현 및 로깅 인터셉터 연결

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,13 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 import mikroOrmConfig from './common/config/mikro-orm.config';
 import { DartModule } from './dart/dart.module';
 import { CrawlerModule } from './crawler/crawler.module';
 import { CompanyModule } from './company/company.module';
+import { RequestIdMiddleware } from './common/request-id.middleware';
+import { HttpLoggingInterceptor } from './common/http-logging.interceptor';
 
 @Module({
   imports: [
@@ -17,6 +20,15 @@ import { CompanyModule } from './company/company.module';
     CompanyModule,
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: HttpLoggingInterceptor,
+    },
+  ],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(RequestIdMiddleware).forRoutes('*');
+  }
+}
---
+++ backend/src/common/request-id.middleware.ts
@@ -0,0 +1,11 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+import { randomUUID } from 'crypto';
+
+@Injectable()
+export class RequestIdMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: NextFunction) {
+    req['requestId'] = randomUUID();
+    next();
+  }
+}
